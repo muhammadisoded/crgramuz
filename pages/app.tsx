0e17f43b26e33fbf758bf7a937888097de32e43b
muhammadisoded <killerlaziz@gmail.com>
2025-12-30 15:34:50 +0500

    Initialize App component with SessionProvider

---
+++ pages/app.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: any) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
