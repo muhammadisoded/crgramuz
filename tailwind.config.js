12120ca33c1127894420329701f2a033371a9f13
muhammadisoded <killerlaziz@gmail.com>
2025-12-30 15:12:45 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        crPrimary: "#00E5FF",
+        crAccent: "#FF2EC4",
+        crBg: "#0B0B0B"
+      }
+    }
+  },
+  plugins: []
+};
